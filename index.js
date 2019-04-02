a3e1333656db3c77b44fae3767eaee8f7afa4c1a
Jakub Lamprecht <jakub.lamprecht@cognifide.com>
2019-04-02 16:38:46 +0200

    Changed export to a function as required by babel

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports = () => ({
     presets: ["@babel/preset-env"],
     plugins: [
         [
@@ -10,4 +10,4 @@ module.exports = {
             { loose: true },
         ],
     ],
-}
+})
